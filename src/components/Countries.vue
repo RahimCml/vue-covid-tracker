a3a7b656783e285a02661c72b780e5a5d46bdf61
Rahim <rhmcml@mail.ru>
2021-06-28 23:08:42 +0400

    Create a vue file named Countries and Country

---
+++ src/components/Countries.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  props: ['data']
+}
+</script>
+
+<template>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Ölkə</th>
+            <th scope="col">Sağalma</th>
+            <th scope="col">Ölüm halları</th>
+            <th scope="col">Yoluxma</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(item, index) in data" :key="index">
+            <th scope="row">{{index + 1}}</th>
+            <td>{{item.Country}}</td>
+            <td>{{item.TotalConfirmed | numberFormat}}</td>
+            <td>{{item.TotalDeaths | numberFormat}}</td>
+            <td>{{item.TotalRecovered | numberFormat}}</td>
+        </tr>
+        </tbody>
+    </table>
+</template>
---
+++ src/components/Country.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  props: ['data']
+}
+</script>
+
+<template>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Ölkə</th>
+            <th scope="col">Sağalma</th>
+            <th scope="col">Ölüm halları</th>
+            <th scope="col">Yoluxma</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(item, index) in data" :key="index">
+            <th scope="row">{{index + 1}}</th>
+            <td>{{item.Date | dateFormat}}</td>
+            <td>{{item.Confirmed | numberFormat}}</td>
+            <td>{{item.Deaths | numberFormat}}</td>
+            <td>{{item.Recovered | numberFormat}}</td>
+        </tr>
+        </tbody>
+    </table>
+</template>
