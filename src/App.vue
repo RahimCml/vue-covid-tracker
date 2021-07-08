0d147e16baa77a255926bbedf3494f08c3f49f91
RahimCml <rhmcml@mail.ru>
2021-07-09 00:28:58 +0400

    Add css for templates

--- src/App.vue
+++ src/App.vue
@@ -101,4 +101,38 @@ export default {
   body {
       background-image: url("https://wallpaperaccess.com/full/1548134.jpg");
   }
+  table {
+    background: #fff;
+  }
+  .container {
+    margin-top: 40px;
+  }
+  .stats-item {
+    padding: 20px;
+    text-align: center;
+    margin-bottom: 20px;
+  }
+  .btn-danger {
+    background-image: url(https://wgntv.com/wp-content/uploads/sites/5/2021/02/GettyImages-1292617439-1.jpg?w=2560&h=1440&crop=1);
+    background-position: 54% 51%;
+    background-size: 359px 184px;
+  }
+  .btn-success {
+    background-image: url(https://ihpi.umich.edu/sites/default/files/styles/hero_slide/public/2020-03/GettyImages-1202969008-yellow_0.jpg?itok=YhuJvnYE);
+    background-position: 132% 42%;
+    background-size: 350px 153px;
+  }
+  .btn-info {
+    background-image: url(https://www.biofin.org/sites/default/files/content/news_media/cvd_1.jpg);
+    background-position: -2px;
+  }
+  .item-title {
+    display: block;
+    color: #fff;
+    font-size: 17px;
+  }
+  .item-count {
+    font-weight: bold;
+    font-size: 30px;
+  }
 </style>
